9a1dd6278cfc3d00c12ce93cafe7762c5bb07e08
Katsutoshi Horie <mps299792458@gmail.com>
2016-06-15 16:06:42 +0900

    Fix error message

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,14 +6,12 @@ if [[ ${DOCKER_HOST} == unix://* ]]; then
     SOCKET_FILE=${DOCKER_HOST#unix://}
     if [ ! -S ${SOCKET_FILE} ]; then
         cat >&2 <<-EOT
-ERROR: you need to share your Docker host socket with a volume at $socket_file
-Typically you should run your jwilder/nginx-proxy with: \`-v /var/run/docker.sock:$socket_file:ro\`
-See the documentation at http://git.io/vZaGJ
+ERROR: you need to share your Docker host socket with a volume at ${SOCKET_FILE}
+Typically you should run your hrektts/nginx-proxy with: \`-v /var/run/docker.sock:${SOCKET_FILE}:ro\`
+See the documentation at https://git.io/vo4XR
 EOT
         exit 1
     fi
 fi
-
-NGINX_MAX_UPLOAD_SIZE=${NGINX_MAX_UPLOAD_SIZE:-20m}
 
 exec "$@"
